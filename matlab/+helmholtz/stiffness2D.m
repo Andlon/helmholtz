c838ebd3ee09c81813afd22e0e3f41b03bcfb62f
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2015-10-30 14:50:00 +0100

    Change stiffness function input arguments.

--- matlab/+helmholtz/stiffness2D.m
+++ matlab/+helmholtz/stiffness2D.m
@@ -1,9 +1,8 @@
-function [A, b] = stiffness2D(k, vertices, triangles, g0, source_index)
+function [A, b] = stiffness2D(k, tri, g0, source_index)
 % helmholtz.STIFFNESS TODO: Add documentation
 
 assert(size(vertices, 2) == 2, 'vertices must be an Nx2 matrix.');
 assert(size(triangles, 2) == 3, 'triangles must be a Kx3 matrix.');
-assert(isa(g, 'function_handle'), 'f must be a function handle.');
 
 T = size(triangles, 1);
 N = size(vertices, 1);
@@ -31,8 +30,8 @@ for t = 1:T
         for j = 1:3
             integrand = @(X) ...
                 - k^2 * ...
-                (X * grad(:, i) + basis(1, i)) .* ...
-                (X * grad(:, j) + basis(1, j));
+                (X * basis(2:3, i) + basis(1, i)) .* ...
+                (X * basis(2:3, j) + basis(1, j));
             A_k(i, j) = A_k(i, j) + integration.quadrature2D(P, 4, integrand);
         end
     end
