727d52dfa4b3e7d79c985b58b2b384dd0b4c52a8
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2015-10-30 16:56:14 +0100

    Correct mistake in edgevectors and add plotting

--- matlab/+helmholtz/unit_box.m
+++ matlab/+helmholtz/unit_box.m
@@ -3,10 +3,29 @@ function data = unit_box( N )
 data.tri = mesh.rect_triangulation(-0.5, 0.5, -0.5, 0.5, N, N);
 vertices = data.tri.Points;
 triangles = data.tri.ConnectivityList;
-edgelist = edges(data.tri);
+edgelist = freeBoundary(data.tri);
 
-[A, b] = helmholtz.stiffness2D(1, vertices, triangles, @(x) 0);
+g0 = -10^8;
+
+c = 3*10^8;
+f = 2.4*10^6;
+omega = 2*pi*f;
+k = 10^(8)*omega/c;
+k = 0.0001;
+
+source_index = nearestNeighbor(data.tri,[0,0]);
+
+
+[A, b] = helmholtz.stiffness2D(k, vertices, triangles, g0, source_index);
 data.u = helmholtz.dirichlet2D(A, b, edgelist);
 
-end
+close all
 
+figure
+trimesh(triangles,vertices(:,1),vertices(:,2),data.u)
+title('FEM solution')
+ylabel('y')
+xlabel('x')
+
+
+end
