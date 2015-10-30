ce1ca5317e59406497a1aa4650dd5b38c805eab4
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2015-10-30 13:16:22 +0100

    Added source term g0 in load vecotr, line 16

--- matlab/+helmholtz/stiffness2D.m
+++ matlab/+helmholtz/stiffness2D.m
@@ -1,4 +1,4 @@
-function [A, b] = stiffness2D(k, vertices, triangles, g)
+function [A, b] = stiffness2D(k, vertices, triangles, g0, source_index)
 % helmholtz.STIFFNESS TODO: Add documentation
 
 assert(size(vertices, 2) == 2, 'vertices must be an Nx2 matrix.');
@@ -10,6 +10,10 @@ N = size(vertices, 1);
 
 A = sparse(N, N);
 b = zeros(N, 1);
+
+% Setting the source_index equal to g0 in the load vector,
+% the rest is zero
+b(source_index) = g0;
 
 % Compute contributions by each element
 for t = 1:T
@@ -38,8 +42,10 @@ for t = 1:T
     % local b_k for the current triangle instead of just a function g
     b_k = zeros(3, 1);
     
+    
     A(indices, indices) = A(indices, indices) + A_k;
     b(indices) = b(indices) + b_k;
 end
+
 
 end
