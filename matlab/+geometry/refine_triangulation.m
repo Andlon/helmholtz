afc9fde906c072045f663235b630147e301e7407
Andreas Longva <andrebl@stud.ntnu.no>
2015-11-12 17:57:56 +0100

    Changed refine_triangulation to only search changed triangles

--- matlab/+geometry/refine_triangulation.m
+++ matlab/+geometry/refine_triangulation.m
@@ -5,6 +5,7 @@ Q = geometry.find_poor_triangles(tri, h);
 S = geometry.find_encroached(tri);
 
 while ~isempty(Q) || ~isempty(S)
+    prev_connectivity = tri.ConnectivityList;
     while ~isempty(S)
         constraint = S(1, :);
         % Add midpoint of encroached constraint to triangulation
@@ -13,27 +14,32 @@ while ~isempty(Q) || ~isempty(S)
     end
     
     if ~isempty(Q)
-        q = Q(1);
-        center = tri.circumcenter(q);
+        % Find index q of triangle Q(end, :). Note that calculating the
+        % circumcenter directly through a formula is probably much faster.
+        % Fix this later. For now, we just want this to work.
+        [triangle_is_valid, q] = ismember(Q(end, :), tri.ConnectivityList, 'rows');
+        Q(end, :) = [];
         
-        % Determine if the circumcenter encroaches any of the constraints
-        for e = 1:size(tri.Constraints, 1)
-            constraint = tri.Constraints(e, :);
-            if encroaches(tri, center, constraint)
-                S(end+1, :) = constraint;
+        if triangle_is_valid    
+            center = tri.circumcenter(transpose(q));
+            
+            % Determine if the circumcenter encroaches any of the constraints
+            for e = 1:size(tri.Constraints, 1)
+                constraint = tri.Constraints(e, :);
+                if encroaches(tri, center, constraint)
+                    S(end+1, :) = constraint;
+                end
             end
-        end
-        
-        if isempty(S)
-            % Add circumcenter to triangulation
-            tri.Points(end+1, :) = center;
+            
+            if isempty(S)
+                % Add circumcenter to triangulation
+                tri.Points(end+1, :) = center;
+            end
         end
     end
     
-    % Global update here.
-    % TODO: Can accelerate substantially by only considering
-    % changed triangles/segments
-    Q = geometry.find_poor_triangles(tri, h);
+    searchspace = changed_triangles(tri, prev_connectivity);
+    Q = union(Q, geometry.find_poor_triangles(tri, h, searchspace), 'rows');
 end
 
 end
@@ -46,5 +52,9 @@ radius = norm(b - center);
 
 d = vertex - center;
 result = radius > norm(d);
+end
+
+function [ indices ] = changed_triangles(tri, prev_connectivity)
+[~, indices] = setdiff(tri.ConnectivityList, prev_connectivity, 'rows');
 end
 
