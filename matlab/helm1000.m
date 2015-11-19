c703105bc4843ee0b88254e94da22510d9b571b9
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2015-11-19 18:41:04 +0100

    Verification and data

--- matlab/helm1000.m
+++ matlab/helm1000.m
@@ -1,2 +1,3 @@
-data_1000_real_source = helmholtz.unit_box(1000);
-save('data_1000_real_source.mat','data_1000_real_source');
+ A = load('data_1000_real_source.mat');
+ norm_1000 = integration.computeL2Norm(A.data_fine_real_source);
+ save('norm_1000.mat','norm_1000');
--- matlab/plotErrorNorm.m
+++ matlab/plotErrorNorm.m
@@ -1,9 +1,9 @@
 function [] = plotErrorNorm(norm,N)
 figure
 hold on
-plot(N,norm,'*')
-plot(N,norm,'--')
-title('Error measured in L2')
+plot(N,log(norm),'*')
+plot(N,log(norm),'--')
+title('Logarithm of Error measured in L2')
 xlabel('N')
 ylabel('Error')
 grid on
